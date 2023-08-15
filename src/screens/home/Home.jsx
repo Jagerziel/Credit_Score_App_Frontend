8035e6f824e8e198eba6d19e48b3690c008c040b
krsnamara <martinj.fitzpatrick@gmail.com>
2023-08-15 16:42:57 -0400

    login logout button conditionally rendered with user instance object

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.scss';
-import Nav from '../../components/Navigation/Navigation';
+import NavTest from '../../components/NavTest/NavTest';
+
+// import auth from firebase.js
+// this will allow us to check if the user is logged in or not to our app
+import { auth } from '../../firebase';
+// importing from the node modules
+// onAuthStateChanged is a function will verify if the user is logged in or not
+import { onAuthStateChanged } from 'firebase/auth';
 
 export default function Home() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => setUser(user));
+  }, []);
+
   return (
     <>
-      <Nav />
+      <NavTest user={user} />
       <div>Home</div>
     </>
   );
