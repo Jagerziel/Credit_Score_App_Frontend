3871048f6b3dfa4b0bb31010942aa00157301034
Jagerziel <fromnytosg@gmail.com>
2023-08-14 23:18:19 -0400

    added env file and updated json

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,12 +1,20 @@
 //Import axios
 import axios from "axios";
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+const {
+  BACKEND_LOCAL_PORT
+} = process.env;
+
 // Set API Variable
 let apiUrl;
 // Set URL link
 const apiUrls = {
   // Need to change later on real link
-  production: "TBC",
-  development: "TBC",
+  production: `https://http://localhost:${BACKEND_LOCAL_PORT}/`,
+  development: `https://http://localhost:${BACKEND_LOCAL_PORT}/`,
 };
 // Determine if production or deployment
 if (window.location.hostname === "localhost") {
