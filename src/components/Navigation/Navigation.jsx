7b3268cdf43b3a56c8a8c403831f09638fc1b402
Jagerziel <fromnytosg@gmail.com>
2023-08-15 16:23:12 -0400

    updated navbar dropdown

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+// Import React
+import React, { useState } from 'react'
+
+// Import Data
+import { navItems } from '../../data/data'
+
+// Import CSS
 import './Navigation.scss'
 
 
 export function Navigation () {
+  const [ menuOpen, setMenuOpen ] = useState(false)
+
+  function handleMenu () {
+    setMenuOpen(!menuOpen)
+  }
+
+  console.log(menuOpen)
   return (
     <>
-      <div className='navigation'>Navigation</div>
+      <div className='navigation'>
+        <div className="navigation-left-container">
+          CREDIT SCORE APPLICATION
+        </div>
+        <div 
+          className='navigation-right-container'
+          onClick={handleMenu}
+        >
+          MENU
+        </div>
+
+      </div>
       <div className='navigation-background'></div>
+      <div className='navigation-menu-container'
+        style={{display: menuOpen ? "block" : "none" }}
+      >
+        {navItems.map((item, index) => {
+          return (
+            <div className='navigation-menu-item'>{item.name}</div>
+          )
+        })}
+      </div>
     </>
   )
 }
