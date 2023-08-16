0eb57ebad770e260a87ab03540fa0f4212f2f7ce
Jagerziel <fromnytosg@gmail.com>
2023-08-16 18:24:51 -0400

    Included shell files for Description components

---
+++ src/components/Descriptions/DescLoggedIn.jsx
@@ -0,0 +1,15 @@
+// Import React
+import React, { useEffect, useState , useContext } from 'react';
+// Import Data
+
+// Import CSS
+import './DescLoggedIn.scss';
+
+export default function DescLoggedIn() {
+
+  return (
+    <div className='descLoggedIn-container'>
+        TEST
+    </div>
+  );
+}
---
+++ src/components/Descriptions/DescLoggedOut.jsx
@@ -0,0 +1,15 @@
+// Import React
+import React, { useEffect, useState , useContext } from 'react';
+// Import Data
+
+// Import CSS
+import './DescLoggedOut.scss';
+
+export default function DescLoggedOut() {
+
+  return (
+    <div className='descLoggedOut-container'>
+        TEST
+    </div>
+  );
+}
