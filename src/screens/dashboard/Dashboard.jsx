c949e1a4a764b608b83b2c08fa2a6e2a1e79b7b1
Jagerziel <fromnytosg@gmail.com>
2023-08-16 15:12:17 -0400

    updated chart positioning

--- src/screens/dashboard/Dashboard.jsx
+++ src/screens/dashboard/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Dashboard.scss'
-
+import PieNeedleChart from '../../components/PieNeedleChart/PieNeedleChart'
 
 export default function Dashboard () {
   return (
     <div className='dashboard-container'>
-      Dashboard
+      <div className='dashboard-pie-needle-chart-container'>
+        <PieNeedleChart />
+      </div>
+      <div>hello</div>
     </div>
   )
 }
