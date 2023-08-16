6d33982b508287ba3bc1430f9c6a9e461bd45db3
Jagerziel <fromnytosg@gmail.com>
2023-08-16 19:51:40 -0400

    updated styling of logged out component

--- src/components/Descriptions/DescLoggedOut.jsx
+++ src/components/Descriptions/DescLoggedOut.jsx
@@ -6,21 +6,43 @@ import React, { useEffect, useState , useContext } from 'react';
 import './DescLoggedOut.scss';
 
 export default function DescLoggedOut( { title , description }) {
+  const [ openMenu , setOpenMenu ] = useState(false)
 
   return (
-    <div className='descLoggedOut-container'>
-        <div className='descLoggedOut-left-container'>
-          <div className='descLoggedOut-blue-v'></div>
-          <div className='descLoggedOut-title'>{title}</div>
+    <>
+      {
+        !openMenu ? 
+        <div className='descLoggedOut-container'>
+            <div className='descLoggedOut-left-container'>
+              <div className='descLoggedOut-blue-v'></div>
+              <div className='descLoggedOut-title'>{title}</div>
+            </div>
+            <img 
+              className='down-arrow'
+              src={require("./down-arrow.png")} 
+              height="24px" 
+              width="24px" 
+              alt="Menu"
+              onClick={() => setOpenMenu(true)}
+            />
         </div>
-        <img 
-          className='down-arrow'
-          src={require("./down-arrow.png")} 
-          height="24px" 
-          width="24px" 
-          alt="Menu"
-        />
-        {/* <div>{description}</div> */}
-    </div>
+        :
+        <div className='descLoggedOut-open-container'>
+          <div className='descLoggedOut-blue-h'></div>
+          <div className='descLoggedOut-open-title-container'>
+            <img 
+              className='down-arrow'
+              src={require("./down-arrow.png")} 
+              height="24px" 
+              width="24px" 
+              alt="Menu"
+              onClick={() => setOpenMenu(false)}
+            />
+            <div className='descLoggedOut-open-title'>{title}</div>
+          </div>
+          <div className='descLoggedOut-open-description'>{description}</div>
+        </div>
+      }
+    </>
   );
 }
