8d99ad608dbb991e7b4836e587197d1c4961dfe1
Jagerziel <fromnytosg@gmail.com>
2023-08-16 18:09:40 -0400

    updated pie chart styling

--- src/screens/dashboard/Dashboard.jsx
+++ src/screens/dashboard/Dashboard.jsx
@@ -5,12 +5,16 @@ import PieNeedleChart from '../../components/PieNeedleChart/PieNeedleChart'
 export default function Dashboard () {
   return (
     <div className='dashboard-container'>
-      <div className='dashboard-score-container'>
+      <div className='dashboard-full-chart-container'>
         <div className='dashboard-score-title'>Your Score</div>
         <div className='dashboard-score-result'>000</div>
-      </div>
-      <div className='dashboard-pie-needle-chart-container'>
-        <PieNeedleChart />
+        <div className='dashboard-pie-needle-chart-container'>
+          <PieNeedleChart />
+        </div>
+        <div className='dashboard-bounds-container'>
+          <div>300</div>
+          <div>850</div>
+        </div>
       </div>
       <div>hello</div>
     </div>
