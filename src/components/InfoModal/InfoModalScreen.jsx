94c0edcb1c057d146061c5fc2e1b887104b34221
Jagerziel <fromnytosg@gmail.com>
2023-08-24 11:52:54 -0400

    Modal updated and implemented

--- src/components/InfoModal/InfoModalScreen.jsx
+++ src/components/InfoModal/InfoModalScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './InfoModalScreen.scss'
 
-export default function InfoModalScreen( { open , setOpen } ) {
+export default function InfoModalScreen( { open , setOpen , scoringDesc } ) {
 
   return (
     <div 
@@ -12,10 +12,10 @@ export default function InfoModalScreen( { open , setOpen } ) {
         <div className='infoModalScreen-subcontainer'>
             <div className='infoModalScreen-info-container'>
                 <div className='infoModalScreen-info-title'>
-                    What your rating means to lenders
+                    {scoringDesc.title}
                 </div>
                 <div className='infoModalScreen-info-description'>
-                Credit score ranges and what they mean will vary based on the score model used to calculate them, but they are generally similar to the following: 300-579 (Poor), 580-669 (Fair), 670-739 (Good), 740-799 (Very Good), and 800-850 (Excellent).
+                    {scoringDesc.description}
                 </div>
                 <div 
                     className='infoModalScreen-info-button'
