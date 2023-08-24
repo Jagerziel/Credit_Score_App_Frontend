b480b2008e85290d5a5915d3916e17b49b490066
Jagerziel <fromnytosg@gmail.com>
2023-08-23 20:18:00 -0400

    Included shell code for InfoModalScreen

--- src/components/InfoModal/InfoModal.jsx
+++ src/components/InfoModal/InfoModal.jsx
@@ -1,4 +1,7 @@
+// Import React
 import React, { useState } from 'react'
+// Import Components
+import InfoModalScreen from './InfoModalScreen.jsx'
 
 export default function InfoModal( { score } ) {
     const [ open , setOpen ] = useState(false)
@@ -8,13 +11,15 @@ export default function InfoModal( { score } ) {
     }
 
     return (
-        <img 
-            className="infoModal-questionIcon" 
-            src={require("./interrogation-mark.png")} 
-            alt="?" 
-            height="17px"
-            width="17px"
-            onClick={() => setOpen(!open)}
-        />
+        <>
+            <img 
+                className="infoModal-questionIcon" 
+                src={require("./interrogation-mark.png")} 
+                alt="?" 
+                height="17px"
+                width="17px"
+                onClick={() => setOpen(!open)}
+            />
+        </>
     )
 }
---
+++ src/components/InfoModal/InfoModalScreen.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function InfoModalScreen() {
+    
+  return (
+    <div>InfoModalScreen</div>
+  )
+}
